989b013e74da77ef7e39ce1a431200dc2a22bc18
Rodrigo Rocha <rocha.webdev@gmail.com>
2024-06-10 23:55:36 -0300

    refactor: disable unecessary lint autocommands

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,16 +6,16 @@ vim.api.nvim_create_autocmd("VimEnter", {
 })
 
 -- Crie um grupo de autocomandos para melhor organização
-local format_on_save_group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
-
--- Adicione um autocomando para formatar arquivos TSX, TS, JSX, JS e Lua antes de salvar
-vim.api.nvim_create_autocmd("BufWritePre", {
-	pattern = { "*.tsx", "*.ts", "*.jsx", "*.js", "*.lua" },
-	callback = function()
-		vim.cmd("ALEFix")
-	end,
-	group = format_on_save_group,
-})
+-- local format_on_save_group = vim.api.nvim_create_augroup("FormatOnSave", { clear = true })
+--
+-- -- Adicione um autocomando para formatar arquivos TSX, TS, JSX, JS e Lua antes de salvar
+-- vim.api.nvim_create_autocmd("BufWritePre", {
+-- 	pattern = { "*.tsx", "*.ts", "*.jsx", "*.js", "*.lua" },
+-- 	callback = function()
+-- 		vim.cmd("ALEFix")
+-- 	end,
+-- 	group = format_on_save_group,
+-- })
 
 vim.api.nvim_create_autocmd("InsertLeave", {
 	pattern = { "*.tsx", "*.jsx" }, -- Aplicar a todos os tipos de arquivo
